f215edaf63e54ffdf18f587858b09641f2093dc6
Son2703 <phamtrungson203@gmail.com>
2023-03-16 09:17:43 +0700

    base config

---
+++ configs/base.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#host mobio
+HOST=os.getenv("HOST")
+ADMIN_HOST=os.getenv("ADMIN_HOST")
+
+#redis
+REDIS_NAME = os.getenv("REDIS_NAME")
+REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_PORT = os.getenv("REDIS_PORT")
+
+#mongo
+MONGO_USERNAME=os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD=os.getenv("MONGO_PASSWORD")
