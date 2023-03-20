b2cfac6901487b117ba8c5bab820e924dab796ab
Son2703 <phamtrungson203@gmail.com>
2023-03-20 11:38:27 +0700

    fix

---
+++ src/common/time.py
@@ -0,0 +1,7 @@
+from src.common.constants import TimeConfig
+import pytz
+from datetime import datetime
+
+def timestamp_utc():
+    return datetime.now(tz=pytz.timezone(TimeConfig.UTC_ZONE)).timestamp()
+
