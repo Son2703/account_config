6aaf4f0a15b51ba372e6b2af1e6aaa50b6de88ca
Son2703 <phamtrungson203@gmail.com>
2023-03-20 09:50:59 +0700

    fix base

--- src/models/mongo/base.py
+++ src/models/mongo/base.py
@@ -5,17 +5,22 @@ class Base:
     def __init__(self, col = None) -> None:
         self.col = col
 
-    def filter_one(self, payload):
-        return self.col.find(payload)
+    def filter_one(self, payload = None):
+        return self.col.find_one(payload)
     
-    def filter_all(self, payload):
-        return [x for x in self.col.find(payload)]
+    def find(self, payload = None, skip = None, limit = None):
+        rs = self.col.find(payload)
+        if limit:
+            rs = rs.limit(limit)
+        if skip:
+            rs  = rs.skip(skip)
+        return [x for x in rs]
 
     def update_one(self, query, payload, updater = None):
         if updater:
             payload.update({
                 CommonKey.UPDATE_BY: updater,
-                CommonKey.UPDATE_AT: datetime.now()
+                CommonKey.UPDATE_AT: datetime.timestamp()
             })
             return self.col.update_one(query, payload)
 
@@ -25,9 +30,12 @@ class Base:
                 CommonKey.CREATE_BY: creator,
                 CommonKey.UPDATE_BY: None,
                 CommonKey.CREATE_AT: None,
-                CommonKey.UPDATE_AT: datetime.now()
+                CommonKey.UPDATE_AT: datetime.timestamp()
             })
         return self.col.insert_one(payload)
+    
+    def create_many(self, payload, creator = None):
+        pass
     
     def delete_all(self):
         return self.col.delete_many({})
