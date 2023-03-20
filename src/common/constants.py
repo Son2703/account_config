9ff0c539dd7bb2574f018aa5fd34a79a925235f0
Son2703 <phamtrungson203@gmail.com>
2023-03-20 11:46:42 +0700

    fix

---
+++ src/common/constants.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class TimeConfig(Enum):
+    UTC_ZONE = "UTC"
+
+class Rule(Enum):
+    VAL_NAME = "val_name"
+    VAL_PASS = "val_pass"
+    CHANGE_PASS_MOTH = "change_pass_moth"
+    UNIQUE_OLD_PASS = "unique_old_pass"
+    REQUIRE_CHANGE_PASS = "require_change_pass"
+    UNIQUE_PASS = "unique_pass"
+    LOCK_ACCOUNT = "lock_account"
