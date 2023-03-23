e1f6153a4663caeaf2144cc0d2d3bcf06657e161
Son2703 <phamtrungson203@gmail.com>
2023-03-23 10:46:36 +0700

    col name

--- src/common/constants.py
+++ src/common/constants.py
@@ -15,11 +15,10 @@ class Rule(Enum):
 class Role(Enum):
     ADMIN = "admin"
     USER = "user"
-class AccountRules(Enum):
-   CHANGE_PASS_MOTH = "change_pass_moth"  # thay đổi theo tháng
-   UNIQUE_OLD_PASS = "unique_old_pass" # không trùng pass cũ
-   REQUIRE_CHANGE_PASS = "require_change_pass" # thay đổi lần đầu đăng nhập,
-   UNIQUE = "unique_pass" # mật khẩu không trùng
-   LOCK_ACCOUNT = "lock_account" # khóa tài khoản
-   VALUE_WLOGIN = "value_wlogin" # giá trị lần đang nhâp sai
-   TIME_LOCK = "time_lock" # thời gian khóa tài khoản
+
+class DatabaseName(Enum):
+    COL_RULE = "rules"
+    COL_USER = "users"
+    COL_LIST_PASS_USER = "list_pass_users"
+    COL_MERCHANT_RULE_ASSIGNMENT = "merchant_rule_assignments"
+    COL_MERCHANT = "merchants"
